7514e2ec6fe9fb5d2096aa608fc5ee88500952e6
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-09-20 18:28:02 -0300

    listing repositories

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
 
 import React from 'react'
 
-const Index = ()=>{
+const Index = (props)=>{
     return(
         <>
         <div>
             <h1>Welcome!</h1>
+            <div>{props.currentDate}</div>
+            <pre>{props.repos.map(repo => {
+                return(
+                    <div>
+                        <h3>{repo.full_name}</h3>
+                        <p>Language: {repo.language} / stars: {repo.stargazers_count}</p>
+                    </div>
+                )
+            })}</pre>
         </div>
         </>
     )
 }
-
+export async function getServerSideProps(context){
+    //catch API
+    const resRepos = await fetch('https://api.github.com/users/vituhonorato/repos?sort=update')
+    //turn API in JSON
+    const repos = await resRepos.json()
+    return {
+        props: {
+            currentDate: new Date().toString(),
+            repos
+           
+        }
+    }
+}
 export default Index
