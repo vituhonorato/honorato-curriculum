444671e04331beb50606fc1df0b45caf2f548e2a
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-09-20 15:12:32 -0300

    create a project with a NextJS

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Index = ()=>{
+    return(
+        <>
+        <div>
+            <h1>Welcome!</h1>
+        </div>
+        </>
+    )
+}
+
+export default Index
