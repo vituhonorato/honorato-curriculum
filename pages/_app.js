b19c57a40a3ed0df230c51c926350a7ad6c38f07
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-09-21 15:19:37 -0300

    Add custom fonts from google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,18 @@ import '../styles/global.css'
 class MyApp extends App {
     render() {
         const {Component, pageProps } = this.props
-        return <Component {...pageProps} />
+        return (
+            <React.Fragment>
+                <style jsx global>
+                    {`
+                     @import url('https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;700&display=swap');
+                    `}
+                  
+                  </style>
+                <Component {...pageProps} />
+            </React.Fragment>
+        
+        ) 
     }
 }
 export default MyApp
