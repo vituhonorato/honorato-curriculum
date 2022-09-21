11504d3f794fa5c91c12a77c65e709a8a63d54ae
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-09-21 07:24:01 -0300

    filter data to send to the front end

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react'
 
-const Index = (props)=>{
+const Index = (props) => {
     return(
         <>
         <div>
@@ -9,12 +9,13 @@ const Index = (props)=>{
             <div>{props.currentDate}</div>
             <pre>{props.repos.map(repo => {
                 return(
-                    <div>
+                    <div key={repo.id}>
                         <h3>{repo.full_name}</h3>
                         <p>Language: {repo.language} / stars: {repo.stargazers_count}</p>
                     </div>
                 )
             })}</pre>
+            
         </div>
         </>
     )
@@ -23,11 +24,35 @@ export async function getServerSideProps(context){
     //catch API
     const resRepos = await fetch('https://api.github.com/users/vituhonorato/repos?sort=update')
     //turn API in JSON
-    const repos = await resRepos.json()
+    const originalRepos = await resRepos.json()
+
+    //repositories list
+    const dontShowRepos = ['']
+
+    //dont show forks repositories
+    const isNotFork = repo => !repo.fork
+
+    //dont show selected repositories
+    const dontShowFilter = repo => dontShowRepos.indexOf(repo.full_name) === -1
+    //filter datas to send to the front end
+    const extractData = repo => ({
+        id: repo.id,
+        full_name: repo.full_name,
+        language: repo.language,
+        stargazers_count: repo.stargazers_count
+    })
+    
+    
+
+    //Filter
+    const repos = originalRepos.filter(dontShowFilter).map(extractData)
+
     return {
         props: {
             currentDate: new Date().toString(),
             repos
+            
+            
            
         }
     }
