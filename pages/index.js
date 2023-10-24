29067dd8b7ae402808e608f4e0919811cad9526c
vituhonorato <vituhonorato.89@gmail.com>
2023-10-24 02:40:58 -0300

    fix responsive design

--- pages/index.js
+++ pages/index.js
@@ -12,31 +12,31 @@ const Index = ({repos, user}) => {
         <>
        
         <div>
-         <div className="container h-screen mx-auto bg-transparent opacity-75 mt-8 mb-8 rounded-lg " >
+         <div className="container h-screen mx-auto bg-transparent opacity-75 mt-8 mb-8 rounded-lg justify-center align-middle " >
             <div className=''>
                 <div className="grid grid-flow-col auto-cols-max justify-end py-3 px-3  space-x-1">
-                    <div className='pt-2 opacity-75  hover:opacity-100'>
+                    <div className='pt-3 px-1 opacity-75  hover:opacity-100'>
                         <Link href="https://github.com/vituhonorato" >
                             <a target="_blank">
-                                <img className=' bg-white rounded-full' src='/images/github.png' alt='github' height="60px" width="60px"/>
+                                <img className=' flex w-6 h-6 md:w-10 md:h-10 lg:w-16 lg:h-16  bg-white rounded-full' src='/images/github.png' alt='github' height="" width=""/>
                             </a>
                            
                         </Link>
                     </div>
-                    <div className='pt-3 pr-1'>
+                    <div className='pt-3 px-1 pr-1'>
                     
                     <Link href="https://www.linkedin.com/in/jvmh/" target="_blank">
                         <a target="_blank">
-                            <img className='opacity-75  hover:opacity-100' src='/images/linkedin.png' alt='linkedin' height="50px" width="50px"/>
+                            <img className='flex w-6 h-6 md:w-10 md:h-10 lg:w-16 lg:h-16 opacity-75  hover:opacity-100' src='/images/linkedin.png' alt='linkedin' height="" width=""/>
                         </a>
                     
                     </Link>
                    </div> 
-                   <div className='pt-3 opacity-75  hover:opacity-100'>
+                   <div className='pt-3 px-1 opacity-75  hover:opacity-100'>
                     
                     <Link href="https://api.whatsapp.com/send?phone=5571987840407&text=Hi%20There,%20how%20can%20i%20help?" target="_blank">
                         <a target="_blank">
-                            <img src='/images/whatsapp.png' alt='whatsapp' height="50px" width="50px"/>
+                            <img className='flex w-6 h-6 md:w-10 md:h-10 lg:w-16 lg:h-16' src='/images/whatsapp.png' alt='whatsapp' height="" width=""/>
                         </a>
                     
                     </Link>
@@ -45,10 +45,10 @@ const Index = ({repos, user}) => {
                 
                  </div>
                 </div>
-                <div className='grid grid-cols-1 gap-4 flex'>
+                <div className='grid grid-cols-1 gap-4 flex justify-content'>
                     <div>
-                        <h1 className="text-4xl pt-3 text-white  text-center"><span className='font-bold'>Hi There</span>, I'm João Victor Marques Honorato</h1><br/>
-                        <h2 className='font-bold font-righteous text-white text-5xl  text-center'>Web Developer</h2>
+                        <h1 className="text-xl md:text-2xl lg:text-3xl pt-3 text-white  text-center"><span className='font-bold'>Hi There</span>, I'm João Victor Marques Honorato</h1><br/>
+                        <h2 className='font-bold font-righteous text-white text-xl md:text-2xl  lg:text-4xl  text-center'>Web Developer</h2>
                     </div>
 
                     <div className='grid justify-items-center'>
@@ -61,7 +61,7 @@ const Index = ({repos, user}) => {
                 
                     <div></div>
                     
-                    <div className='text-center text-white px-3 text-xl font-semibold'>
+                    <div className='text-center text-white px-3 text-base  md:text-lg  lg:text-xl font-semibold'>
                     I am a Fullstack WEB developer who is always looking for new knowledge and experiences to enhance my career and contribute to the developer community. I am always open to challenges and willing to learn new technologies to create efficient and elegant solutions. My goal is to become an increasingly complete professional and able to offer solutions that meet the needs of customers and end users.
                     </div>
                     {/* <div className='text-center'> 
@@ -77,18 +77,18 @@ const Index = ({repos, user}) => {
                     </div>
 
                     <div className="flex flex-row  justify-center ">
-                        <div ><img src='/images/html.png' alt='html' height="50px" width="50px"/></div>
-                        <div><img src='/images/css.png' alt='css' height="50px" width="50px"/></div>
-                        <div><img src='/images/js.png' alt='js' height="50px" width="50px"/></div>
-                        <div><img src='/images/typescript.png' alt='typescript' height="50px" width="50px"/></div>
-                        <div><img src='/images/react.png' alt='react' height="50px" width="50px"/></div>
-                        <div><img src='/images/angular.png' alt='angular' height="50px" width="50px"/></div>
-                        <div className='py-4'><img src='/images/tailwind.png' alt='tailwind' height="50px" width="50px"/></div>
-                        <div><img src='/images/node-js.png' alt='node' height="50px" width="50px"/></div>
-                        <div><img src='/images/nest.png' alt='nest' height="50px" width="50px"/></div>
-                        <div><img src='/images/graphql.png' alt='graphql' height="50px" width="50px"/></div>
-                        <div><img className=' bg-white rounded-full py-1' src='/images/rest.png' alt='rest' height="50px" width="50px"/></div>
-                        <div><img src='/images/postgresql.png' alt='postgresql' height="50px" width="50px"/></div>
+                        <div className='flex w-6 h-6 md:w-10 md:h-10 lg:w-16 lg:h-16' ><img src='/images/html.png' alt='html' height="" width=""/></div>
+                        <div className='flex w-6 h-6 md:w-10 md:h-10 lg:w-16 lg:h-16'><img src='/images/css.png' alt='css' height="" width=""/></div>
+                        <div className='flex w-6 h-6 md:w-10 md:h-10 lg:w-16 lg:h-16'><img src='/images/js.png' alt='js' height="" width=""/></div>
+                        <div className='flex w-6 h-6 md:w-10 md:h-10 lg:w-16 lg:h-16'><img src='/images/typescript.png' alt='typescript' height="" width=""/></div>
+                        <div className='flex w-6 h-6 md:w-10 md:h-10 lg:w-16 lg:h-16'><img src='/images/react.png' alt='react' height="" width=""/></div>
+                        <div className='flex w-6 h-6 md:w-10 md:h-10 lg:w-16 lg:h-16'><img src='/images/angular.png' alt='angular' height="" width=""/></div>
+                        <div className='flex p-1 w-6 h-6 md:w-10 md:h-10 lg:w-16 lg:h-16'><img src='/images/tailwind.png' alt='tailwind' height="" width=""/></div>
+                        <div className='flex w-6 h-6 md:w-10 md:h-10 lg:w-16 lg:h-16'><img src='/images/node-js.png' alt='node' height="" width=""/></div>
+                        <div className='flex w-6 h-6 md:w-10 md:h-10 lg:w-16 lg:h-16'><img src='/images/nest.png' alt='nest' height="" width=""/></div>
+                        <div className='flex w-6 h-6 md:w-10 md:h-10 lg:w-16 lg:h-16'><img src='/images/graphql.png' alt='graphql' height="" width=""/></div>
+                        <div className='flex w-6 h-6 md:w-10 md:h-10 lg:w-16 lg:h-16'><img className=' bg-white rounded-full py-1' src='/images/rest.png' alt='rest' height="" width=""/></div>
+                        <div className='flex w-6 h-6 md:w-10 md:h-10 lg:w-16 lg:h-16'><img src='/images/postgresql.png' alt='postgresql' height="" width=""/></div>
                         
                     </div>
                 
